48bcdd7ce151d3876c0caa7b070f2eb74fd30664
phapli <phapli1991@gmail.com>
2017-11-26 00:25:00 +0700

    add market app

---
+++ IosApplication/pPointMarket/pPointMarket/ProductCell.swift
@@ -0,0 +1,14 @@
+//
+//  ProductCell.swift
+//  pPointMarket
+//
+//  Created by Tuyen on 11/25/17.
+//  Copyright © 2017 Tuyen. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var product: UIImageView!
+}
