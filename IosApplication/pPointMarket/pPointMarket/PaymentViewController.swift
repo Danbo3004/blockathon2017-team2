e15134530083a3a1b7ee2316a2ebf1460275f32e
phapli <phapli1991@gmail.com>
2017-11-26 00:32:42 +0700

    add RSA function

--- IosApplication/pPointMarket/pPointMarket/PaymentViewController.swift
+++ IosApplication/pPointMarket/pPointMarket/PaymentViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SwiftyRSA
 
 class PaymentViewController: UIViewController {
     
@@ -14,7 +15,12 @@ class PaymentViewController: UIViewController {
     @IBOutlet weak var detailImg: UIImageView!
     var isLoyalty: Bool = true
     let serverUrl: String = "192.168.22.4:3000"
-    let myAddress:String = ""
+    let myAddress: String = "0x1ABbFe3E2F17E9b8eF5B63383CdAef963b7c4dcF"
+    
+    let customerAddress: String = "0x2EA1C8AF1E7aa5c8Fc572eB4125d72Ab9486E586"
+    
+    let publicKeyString = "MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAJNBr6x8U7Z/75jQ+l5vQ4J8h1I2nLbdrvTKqoVy+WmXvDdaCUh/ZkXRQOCTI+zlAbx4eCtS12eL6V+moYIOHxECAwEAAQ=="
+    let privateKeyString = "MIIBVAIBADANBgkqhkiG9w0BAQEFAASCAT4wggE6AgEAAkEAk0GvrHxTtn/vmND6Xm9DgnyHUjactt2u9MqqhXL5aZe8N1oJSH9mRdFA4JMj7OUBvHh4K1LXZ4vpX6ahgg4fEQIDAQABAkA7V+5e+Z+W3YoGMLmlUvuG236BUEbpnv8B4abEruf6eC704ihORRvEZFwcJfwwY9VT+AAL3t+n+2vI4rU1MIZNAiEA7k8cgwxon+FSrjU4uwI/JArGY3ElaqjhfHVj0UTIvgcCIQCeMDCaEm82gX6T57XKoJzzg8viJpp0mPMk4knjbDJ0JwIhAMh+4rwW2od46TkMKficgUbvkc3kSdic0WkG0r4kIshNAiAqghKdZHTENdoYzrw4Ph7PBUylcfAxguRbtf9bALkyIwIgHPdkalh1hm9uFfozRoknFDOsV9qX+010Y5ujjQVnTNc="
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -68,6 +74,43 @@ class PaymentViewController: UIViewController {
             print(error.localizedDescription)
 
         }
+    }
+    
+    func decryptData(cypherText: String) -> String {
+        
+        do {
+            let privateKey = try PrivateKey(base64Encoded: privateKeyString)
+            let encrypted = try EncryptedMessage(base64Encoded: cypherText)
+            let clear = try encrypted.decrypted(with: privateKey, padding: .PKCS1)
+            
+            // Then you can use:
+            let data = clear.data
+            let base64String = clear.base64String
+            let string = try! clear.string(encoding: .utf8)
+            
+            print(data)
+            print(base64String)
+            print(string)
+            return string
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+        return ""
+    }
+    
+    func encryptData(plainText: String) -> String {
+        
+        let publicKey = try! PublicKey(base64Encoded: publicKeyString)
+        let clear = try! ClearMessage(string: plainText, using: .utf8)
+        let encrypted = try! clear.encrypted(with: publicKey, padding: .PKCS1)
+        
+        // Then you can use:
+        let data = encrypted.data
+        let base64String = encrypted.base64String
+        
+        print(data)
+        print(base64String)
+        return base64String
     }
 
     /*
